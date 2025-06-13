fc31053d09967b43c700f79d44b82cec373b49f3
frshmt89 <143622478+frshmt89@users.noreply.github.com>
2025-06-13 20:59:50 +0200

    added a count function to stats.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import wordcount
+from stats import convert
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -11,6 +12,7 @@ def get_book_text(filepath):
 book_name = get_book_text("./books/frankenstein.txt")
 num_words = wordcount(book_name)
 print(f"{num_words} words found in the document")
+print(convert(book_name))
 
 # def main():
 #     print(get_book_text("./books/frankenstein.txt"))
--- stats.py
+++ stats.py
@@ -1,2 +1,13 @@
 def wordcount(text):
     return len(text.split())
+
+def convert(book):
+    count = {}
+    for char in book:
+        char = char.lower()
+        # print(char)
+        if char in count:
+            count[char] += 1
+        else:
+            count[char] = 1
+    return count
